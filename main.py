a636b3d802ae9ce82c94ac008010ae652b18829b
saurav shrestha <sauravshrestha@sauravs-MacBook-Air.local>
2023-09-13 20:49:51 +0545

    Added data transformation pipeline

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from textSummarization.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 # from textSummarization.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
-# from textSummarization.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
+from textSummarization.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 # from textSummarization.pipeline.stage_04_model_training import ModelTrainingPipeline
 # from textSummarization.pipeline.stage_05_model_evaluation import ModelEvaluationPipeline
 from textSummarization.logging import logger
@@ -16,3 +16,14 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME = "Data Transformation stage"
+try:
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<") 
+   data_transformation = DataTransformationTrainingPipeline()
+   data_transformation.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
